b2b6df8a4e57360e4cdc77f68110d5e2af7d75f9
Jorge Garcia <jgs_23072000@outlook.com>
2024-01-28 23:35:29 -0800

    routing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 //subscription is a way to subscribe to an observable different from the one we are in
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -14,7 +15,7 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean = false;
   subscription: Subscription;
 
-  constructor(private ui: UiService) {
+  constructor(private ui: UiService, private router: Router) {
     this.subscription = this.ui.onToggle().subscribe((value) => this.showAddTask = value);
     //we are subscribing to the observable that is returned from the onToggle() method
     //and we are setting the value of showAddTask to the value that is returned from the observable
@@ -25,7 +26,9 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTask() {
     this.ui.toggleAddTask();
-
   }
 
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
 }
