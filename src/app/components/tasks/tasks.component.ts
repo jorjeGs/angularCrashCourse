9cc696b7e4f9ab217d76571e536a3e42717eb1e6
jorjeGs <jgs_23072000@outlook.com>
2024-01-28 20:11:08 -0800

    first checkpoint

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+import { TaskService } from '../../services/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = this.taskService.getTasks();
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+  }
+
+}
